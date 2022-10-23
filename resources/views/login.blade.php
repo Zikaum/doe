9c6b8c3cacda9544bc0a0e9d369025cafe8b4049
Zikaum <34901594+Zikaum@users.noreply.github.com>
2022-10-23 13:10:12 -0300

    Migrando para laravel
    Tabela users criada, crud de cadastro básico implementado, telas de cadastro, e deseja doar implementadas

---
+++ resources/views/login.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}" />
+    <title>Doe</title>
+</head>
+<body style="margin: 0px;">
+
+    <header>
+        <img id="header-image" src="{{asset('images/DoeAgora.png')}}" alt="Doe Agora"/>
+        <nav>
+            <div style="display: flex; flex: 1; justify-content: center;">
+                <a href="/">INICIO</a>
+                <a href="/requirements">DESEJA DOAR?</a>
+            </div>
+            <div class="login">
+                <div>
+                    <img width="40" src="{{asset('images/GroupIcon.png')}}" alt="Cadastre-se" />
+                    <a href="/register">CADASTRE-SE</a>
+                </div>
+                <div class="a_selected">
+                    <img width="30px" src="{{asset('images/ProfileIcon.png')}}" alt="Entre" />
+                    <a href="/login">ENTRAR</a>
+                </div>
+            </div>
+        </nav>
+      </header>
+
+
+    login
+
+    <footer></footer>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/requirements', function () {
+    return view('requirements');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::post('/register', function (Request $request) {
+    User::create([
+        "name" => $request->name,
+        "pass" => $request->pass,
+        "blood_type" => $request->bloodType,
+        "cep" => $request->cep,
+        "state" => $request->state,
+        "city" => $request->city,
+        "age" => $request->age,
+        "email" => $request->email
+    ]);
+
+    return view('register');
+});
+
