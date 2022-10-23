68bb80fc2d3bf8a2fcf8fb4cf3a01602bdb3230d
Zikaum <34901594+Zikaum@users.noreply.github.com>
2022-10-23 15:01:05 -0300

    Pagina de login adicionada

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
     <title>Doe</title>
 </head>
 <body style="margin: 0px;">
@@ -30,8 +31,26 @@
         </nav>
       </header>
 
-
-    login
+      <div class="main">
+          <div style="display: flex; justify-content: center; align-items: center;">
+            <form action="/login" method="POST">
+                <h1>LOGIN</h1>
+                <p>
+                    <label for="email">Email: </label>
+                    <input type="text" name="email">
+                </p>
+                <p>
+                    <label for="pass">Senha: </label>
+                    <input type="password" name="pass">
+                </p>
+                <button>
+                    <img src="{{asset('images/ProfileIcon.png')}}" width="25" alt="ProfileIcon">
+                    <p>ENTRAR</p>
+                </button>
+            </form>
+            <img src="{{asset('images/Doe.png')}}" width="150" alt="Doe">
+        </div>
+      </div>
 
     <footer></footer>
 </body>
